b3a9562089df4358ad97be25f104ec489e417e9b
Leo <60847511+Kaglion@users.noreply.github.com>
2025-11-04 11:33:38 +0100

    Ajoute du role admin

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -5,6 +5,7 @@ namespace App\Enums;
 enum Role: string
 {
     case SUPER_ADMIN = 'super_admin';
+    case ADMIN = 'admin';
     case MANAGER = 'manager';
     case RESPONSABLE_N1 = 'responsable_n1';
     case RESPONSABLE_N2 = 'responsable_n2';
@@ -15,6 +16,7 @@ enum Role: string
     {
         return match($this) {
             self::SUPER_ADMIN => 'Super Administrateur',
+            self::ADMIN => 'Administrateur',
             self::MANAGER => 'Manager',
             self::RESPONSABLE_N1 => 'Responsable N1',
             self::RESPONSABLE_N2 => 'Responsable N2',
@@ -27,9 +29,15 @@ enum Role: string
     {
         return match($this) {
             self::SUPER_ADMIN => ['*'], // All permissions
-            self::MANAGER => [
+            self::ADMIN => [
                 'projets.create', 'projets.update', 'projets.delete',
                 'activites.create', 'activites.update', 'activites.delete',
+                'taches.create', 'taches.update', 'taches.delete',
+                'users.view', 'users.assign', 'reports.view'
+            ],
+            self::MANAGER => [
+                'projets.create', 'projets.update',
+                'activites.create', 'activites.update',
                 'taches.create', 'taches.update', 'taches.delete',
                 'users.view', 'users.assign', 'reports.view'
             ],
