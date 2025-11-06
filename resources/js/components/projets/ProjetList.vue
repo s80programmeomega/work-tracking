6302d3fc8a60c2ed36b7822f5e59d961d658df45
Leo <60847511+Kaglion@users.noreply.github.com>
2025-11-06 14:16:42 +0100

    Update ProjetList.vue

--- resources/js/components/projets/ProjetList.vue
+++ resources/js/components/projets/ProjetList.vue
@@ -207,8 +207,7 @@
                 >
                   <button
                     @click.stop="editProjet(projet)"
-                    class="w-full flex items-center gap-3 px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-600 rounded-t-lg"
-                  >
+                    class="w-full flex items-center gap-3 px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-600 rounded-t-lg" >
                     <EditIcon class="w-4 h-4" />
                     Modifier
                   </button>
