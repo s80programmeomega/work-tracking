b40ff3cb98a9bb4f99310a633b3e78330243b2b4
Leo <60847511+Kaglion@users.noreply.github.com>
2025-11-07 16:41:32 +0100

    feat: add missing dashboard icons
    - Add ArrowRightIcon for navigation
    - Add TrendingDownIcon for negative trends
    - Add FolderKanbanIcon for projects
    - Add ListTodoIcon for tasks
    - Add TargetIcon for completion rates
    - Update icons index with new exports

---
+++ resources/js/icons/ArrowRightIcon.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    v-bind="$attrs"
+  >
+    <path stroke-linecap="round" stroke-linejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+  </svg>
+</template>
---
+++ resources/js/icons/TrendingDownIcon.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    v-bind="$attrs"
+  >
+    <path stroke-linecap="round" stroke-linejoin="round" d="M13 17h8m0 0V9m0 8l-8-8-4 4-6-6" />
+  </svg>
+</template>
